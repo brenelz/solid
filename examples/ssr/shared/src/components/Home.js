509c1cdfa909e8719375b37bcf57e4b77d0916b8
Brenley Dueck <brenleydueck@gmail.com>
2026-02-26 16:01:05 -0600

    example with show

--- examples/ssr/shared/src/components/Home.js
+++ examples/ssr/shared/src/components/Home.js
@@ -15,12 +15,27 @@ const Home = () => {
       <h1>Welcome to this Simple Routing Example</h1>
       <p>Click the links in the Navigation above to load different routes.</p>
       <span>{s()}</span>
+      <Counter />
     </Wrapper>
   );
 };
 
 function Wrapper(props) {
   return <>{props.children}</>;
+}
+
+function Counter() {
+  const [count, setCount] = createSignal(0);
+  return (
+    <div>
+      <Show when={count() < 10} fallback={<div>Too many clicks</div>}>
+        <div>
+          <span>{count()}</span>
+          <button onClick={() => setCount(count() + 1)}>Click me</button>
+        </div>
+      </Show>
+    </div>
+  );
 }
 
 export default Home;
